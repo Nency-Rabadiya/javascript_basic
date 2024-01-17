7250ad1c4b6640250ec2af14a7931d71623370d6
nency <nency.rabadiya@exhibytesolution.com>
2024-01-17 17:17:25 +0530

    javascript basic topic coverd

---
+++ arrr.js
@@ -0,0 +1,91 @@
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = 'lemon';
+
+fruits.push("kiwi")
+console.log(fruits);
+const update= fruits.toString().slice(7,13);
+
+console.log(update);
+*/
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.copyWithin(3,1);
+
+console.log(fruits);
+*/
+
+/*
+const myArr = [[1,2],[3,4],[5,6],[7,8],[9,10]];
+const newArr = myArr.flat();
+
+console.log(newArr);
+*/
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+console.log(fruits);
+*/
+
+
+/*
+const numbers = [6,7,3,9,53,19,87];
+
+const newNumber = numbers.find(function (ele){
+ return ele > 53;
+});
+
+console.log(newNumber);
+*/
+
+
+//---------------SORTING-ascending
+
+
+/*
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){
+  return a-b
+});
+console.log(points);
+*/
+
+
+//-----------------SORTING-decending
+
+/*const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){
+  return b-a
+});
+console.log(points);
+*/
+
+//------------------ MIN-MAX value
+
+
+/*
+let point = [40, 100, 1, 5, 25, 10];
+let minValue = Math.min(...point);
+let maxValue = Math.max(...point);
+console.log(minValue);
+console.log(maxValue);
+*/
+
+//------------------OBJECT sorting
+
+/*
+const cars = [
+  { type: "Volvo", year: 2016 },
+  { type: "Saab", year: 2001 },
+  { type: "BMW", year: 2010 }
+];
+
+let newUpdate = cars.sort(function (a, b) {
+  return a.year - b.year
+});
+
+console.log(newUpdate);
+*/
---
+++ object.js
@@ -0,0 +1,36 @@
+//----------------------example1 (PROTOTYPE)-------------------------//
+/*function book(tittle , author){
+  this.tittle = tittle;
+  this.author = author;
+}
+
+book.prototype.bookDetail =function (){
+  console.log(`tittle of book is ${this.tittle} and author of book is ${this.author}`);
+}
+
+let book1 = new book("abc",'xyz');
+let book2 = new book("nop",'qrs');
+
+book1.bookDetail();
+book2.bookDetail();
+*/
+
+//----------------------example1 (PROTOTYPE)-------------------------//
+
+/*
+function Person(name, job, yearOfBirth){    
+  this.name= name; 
+  this.job= job; 
+  this.yearOfBirth= yearOfBirth; 
+} 
+
+Person.prototype.calculateAge= function(){ 
+  console.log(`The current age of ${this.name} is: ${(2024- this.yearOfBirth)}`); 
+} 
+let person1 = new Person('nency' , 'intern' , 2003);
+let person2 = new Person('lipsa' , 'intern' , 2002);
+
+person1.calculateAge();
+person2.calculateAge();
+*/
+
