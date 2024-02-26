0f41ca4e698d82df72cbb5e83a9fb8128d7c9672
nency <nency.rabadiya@exhibytesolution.com>
2024-02-26 18:47:51 +0530

    Added display employee page in that add page added add employee button , delete employee button , and log out button .

--- website/logInScript.js
+++ website/logInScript.js
@@ -16,6 +16,61 @@ function validation() {
   if (password.length < 8) {
     console.log("Password must contain 8 character.")
   }
-  window.location.href = "successPage.html";
+  const apiUrl = "http://localhost:8000/"; // Replace with your API endpoint
+
+  const formData = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
+
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Handle the successful response
+      console.log("successfully added", data);
+    })
+    .catch((error) => {
+      // Handle errors during the fetch request
+      console.error("Fetch error:", error);
+    });
 }
+const formData = {
+  username: username,
+  password: password
+};
+
+fetch("http://localhost:8000/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(formData),
+})
+
+  .then((response) => {
+    if (!response) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Handle the successful response
+    console.log("success");
+    console.log(data);
+  })
+  .catch((error) => {
+    // Handle errors during the fetch request
+    console.error("Fetch error:", error);
+  });
 
